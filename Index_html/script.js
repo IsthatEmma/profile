9fa0ec257ff74fd9b4eccd9087435cf706adaa8c
IsthatEmma <144739374+IsthatEmma@users.noreply.github.com>
2023-11-03 19:33:36 +0000

    Index_html folder

---
+++ Index_html/script.js
@@ -0,0 +1,70 @@
+// PROBLEM ONE
+// Given an object of the stolen items for a household, return the total amount of items stolen (number). If nothing was robbed, return the string "Lucky you!”.
+
+function houseTotal(obj) {
+var socks = 7,
+var spoons = 20,
+var shoelaces = 4,
+for (var i = 0; i < houseTotal; i++) {
+  if(houseTotal [i].item == stolenitems) {
+    socks = socks + housetotal [i].stolen;
+
+  } else if (houseTotal[i].stolenitems == "spoons") {
+    spoons = spoons + houseTotal[i].stolen;
+  
+  } else if (houseTotal[i].stolenitems == "shoelaces") {
+    shoelaces = shoelaces + houseTotal[i].stolen;
+  }
+}
+
+    return (7 + 20 + 4) * (31);
+    
+    // uncomment these out to test it out
+    //console.log(houseTotal(stolenItems[0])); 
+    //console.log(houseTotal(stolenItems[1]));
+    //console.log(houseTotal(stolenItems[2])); 
+     
+    // PROBLEM TWO
+    // Given an object of the stolen items for a household and a string in lowercase representing a specific item, return true if that item was stolen and false if it was not. 
+    
+    function isStolen(obj, item) {
+      var rotisserie_chicken = 1,
+      var keys = 4,
+      var nutellajar = 4,
+      for (var i = 0; i < isStolen; i++) {
+        if(houseTotal [i].item == stolenitems) {
+          rotisserie_chicken = rotisserie_chicken + isStolen [i].stolen;
+
+    } else if (isStolen[i].stolenitems == "keys") {
+      keys = keys + isStolen[i].stolen;
+      
+    } else if (isStolen[i].stolenitems == "nutellajar") {
+      nutellajar = nutellajar + isStolen[i].stolen;
+
+
+
+    return (1 + 4 + 4) * (9)
+    }
+    // uncomment these out to test it out
+    //console.log(houseTotal(stolenItems[0], "keys")); 
+    //console.log(houseTotal(stolenItems[1], "socks"));
+    //console.log(houseTotal(stolenItems[2], "spoons")); 
+    
+    
+    // EXTRA CREDIT!!!!! PROBLEM THREE
+    // Given an array of objects representing an entire neighborhood, return the most frequently stolen item. This is NOT the item that has been stolen from the most households, this is the item that has been stolen the most times TOTAL. 
+    
+    function neighborhoodTotal(arr) {
+    
+      return total; 
+      (1) * (1)
+    }
+    
+    // uncomment these out to test it out
+    //console.log(houseTotal(stolenItems)); 
+    
+    
+    // PROBLEM FOUR
+// Based on the item that has been stolen the most, who do you think the burglar is? (you won’t get marked down for getting the “wrong” answer - just give me a fun explanation for why you think it is who it is! 
+console.log("The total for houseTotal is" + 31);
+console.log("The total of isStolen is" + 9);
